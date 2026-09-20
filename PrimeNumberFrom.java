6972e4e968d0e5640c10fc5eb662b31fb5bb6bff
Ayush Gupta <uhbayush@gmail.com>
2026-09-20 16:43:39 +0530

    Add new java program

--- PrimeNumberFrom.java
+++ PrimeNumberFrom.java
@@ -1,5 +1,5 @@
 import java.util.Scanner;
-public class PrimeNumber {
+public class PrimeNumberFrom-1{
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
         int i,j,num,count = 0;
